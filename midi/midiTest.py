f0da3c19e140c36e72c78a03aebfdc50bbf258d9
bblu <gmwblu@gmail.com>
2017-01-05 00:31:34 +0800

    import ledDriver

--- midi/midiTest.py
+++ midi/midiTest.py
@@ -1,6 +1,19 @@
-#I Fule Python
+# I Fule Python
 
 import midi
+import ledDriver
 pattern = midi.read_midifile('mary.mid')
-print pattern
+print type(pattern)
+#print ledDriver.__name__
+print pattern.format
+print pattern.resolution
+print pattern.count('track')
+track = pattern[1]
+
+for event in track:
+    print event.tick
+
+
+ledDriver.clear()
+
 
