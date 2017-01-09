22b557773a4a802969bfd076fcef59338de20dcf
bblu <gmwblu@gmail.com>
2017-01-09 23:22:58 +0800

    38x2

--- basic/dip74hc138.py
+++ basic/dip74hc138.py
@@ -8,9 +8,7 @@ E1=20
 E2=21
 E3=19
 
-H=True
-L=False
-gpio.setwarnings(False)
+#gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
 gpio.setup(OUT_0,gpio.OUT)
@@ -21,46 +19,36 @@ gpio.setup(E1,gpio.OUT)
 gpio.setup(E2,gpio.OUT)
 gpio.setup(E3,gpio.OUT)
 
-#gpio.output(OUT_0,L)
-#gpio.output(OUT_1,H)
-#gpio.output(OUT_2,H)
+H=True
+L=False
+#gpio.output(E3,H)
+#gpio.output(E2,L)
+#gpio.output(E1,L)
+#   0       1       2       3       4       5       6       7
+RV=[[L,L,L],[H,L,L],[L,H,L],[H,H,L],[L,L,H],[H,L,H],[L,H,H],[H,H,H]]
 
 def outputNum(num):
-    if num==0:
-        gpio.output(OUT_0,L)
-        gpio.output(OUT_1,L)
-        gpio.output(OUT_2,L)
-    elif num==1:
-        gpio.output(OUT_0,H)
-        gpio.output(OUT_1,L)
-        gpio.output(OUT_2,L)
-    elif num==2:
-        gpio.output(OUT_0,L)
-        gpio.output(OUT_1,H)
-        gpio.output(OUT_2,L)
-    elif num==3:
-        gpio.output(OUT_0,H)
-        gpio.output(OUT_1,H)
-        gpio.output(OUT_2,L)
-    elif num==4:
-        gpio.output(OUT_0,L)
-        gpio.output(OUT_1,L)
-        gpio.output(OUT_2,H)
-    elif num==5:
-        gpio.output(OUT_0,H)
-        gpio.output(OUT_1,L)
-        gpio.output(OUT_2,H)
-    elif num==6:
-        gpio.output(OUT_0,L)
-        gpio.output(OUT_1,H)
-        gpio.output(OUT_2,H)
-    elif num==7:
-        gpio.output(OUT_0,H)
-        gpio.output(OUT_1,H)
-        gpio.output(OUT_2,H)
+    gpio.output(OUT_0,RV[num][0])
+    gpio.output(OUT_1,RV[num][1])
+    gpio.output(OUT_2,RV[num][2])
+    #print num, RV[num]
+i=0
+if H:
+    t=0.5
+    outputNum(2)
+    time.sleep(t)
+    #outputNum(1)
+    #time.sleep(t)
+    #outputNum(2)
+    #time.sleep(t)
+    #outputNum(3)
+    
+else:
+    for i in range(12):
+        print i%8,
+        outputNum(i%8)
+        time.sleep(0.5)
 
-
-for i in range(864):
-    outputNum(i%8)
-    time.sleep(0.05)
+#time.sleep(2)
+gpio.cleanup()
     
---
+++ basic/dip74hc138x2.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as gpio
+import time
+OUT_0=2
+OUT_1=3
+OUT_2=4
+OUT_3=5
+
+
+#gpio.setwarnings(False)
+gpio.setmode(gpio.BCM)
+
+gpio.setup(OUT_0,gpio.OUT)
+gpio.setup(OUT_1,gpio.OUT)
+gpio.setup(OUT_2,gpio.OUT)
+gpio.setup(OUT_3,gpio.OUT)
+
+
+H=True
+L=False
+#gpio.output(E3,H)
+#gpio.output(E2,L)
+#gpio.output(E1,L)
+#   0       1       2       3       4       5       6       7
+RV=[[L,L,L,L],[H,L,L,L],[L,H,L,L],[H,H,L,L],[L,L,H,L],[H,L,H,L],[L,H,H,L],[H,H,H,L],
+    [L,L,L,H],[H,L,L,H],[L,H,L,H],[H,H,L,H],[L,L,H,H],[H,L,H,H],[L,H,H,H],[H,H,H,H]]
+
+def outputNum(num):
+    gpio.output(OUT_0,RV[num][0])
+    gpio.output(OUT_1,RV[num][1])
+    gpio.output(OUT_2,RV[num][2])
+    gpio.output(OUT_3,RV[num][3])
+    #print num, RV[num]
+while True:
+    for i in range(16):
+        outputNum(i)
+        time.sleep(0.03)
+
+    
