52ddad4755d6fffb49809d081bfa3fbda2c1e83f
wenbo.Lu <gmwblu@gmail.com>
2017-01-09 23:36:57 +0800

    Update dip74hc138x2.py

--- basic/dip74hc138x2.py
+++ basic/dip74hc138x2.py
@@ -5,8 +5,6 @@ OUT_1=3
 OUT_2=4
 OUT_3=5
 
-
-#gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
 gpio.setup(OUT_0,gpio.OUT)
@@ -14,17 +12,13 @@ gpio.setup(OUT_1,gpio.OUT)
 gpio.setup(OUT_2,gpio.OUT)
 gpio.setup(OUT_3,gpio.OUT)
 
-
 H=True
 L=False
-#gpio.output(E3,H)
-#gpio.output(E2,L)
-#gpio.output(E1,L)
 #   0       1       2       3       4       5       6       7
 RV=[[L,L,L,L],[H,L,L,L],[L,H,L,L],[H,H,L,L],[L,L,H,L],[H,L,H,L],[L,H,H,L],[H,H,H,L],
     [L,L,L,H],[H,L,L,H],[L,H,L,H],[H,H,L,H],[L,L,H,H],[H,L,H,H],[L,H,H,H],[H,H,H,H]]
 
-def outputNum(num):
+def turnOnLED(num):
     gpio.output(OUT_0,RV[num][0])
     gpio.output(OUT_1,RV[num][1])
     gpio.output(OUT_2,RV[num][2])
@@ -32,7 +26,5 @@ def outputNum(num):
     #print num, RV[num]
 while True:
     for i in range(16):
-        outputNum(i)
+        turnOnLED(i)
         time.sleep(0.03)
-
-    
